ee03236ea67289efdbcfdf0d7e3f3e0affe8bca8
Your Name <you@example.com>
2021-04-26 20:26:24 -0500

    Finished Menu and MenuItem classes

---
+++ Restaurant Menu/src/org/launchcode/Menu.java
@@ -0,0 +1,31 @@
+package org.launchcode;
+
+import org.launchcode.MenuItem;
+
+import java.util.Date;
+import java.util.ArrayList;
+
+public class Menu {
+    private Date lastUpdated;
+    private ArrayList<MenuItem> items;
+
+    public Menu(Date d,ArrayList<MenuItem> i) {
+        this.lastUpdated = d;
+        this.items = i;
+    }
+
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    public void setItems(ArrayList<MenuItem> items) {
+        this.items = items;
+    }
+    public Date getLastUpdated(){
+        return lastUpdated;
+    }
+
+    public ArrayList<MenuItem> getItems() {
+        return items;
+    }
+}
---
+++ Restaurant Menu/src/org/launchcode/MenuItem.java
@@ -0,0 +1,31 @@
+package org.launchcode;
+
+public class MenuItem {
+    private double price;
+    private String description;
+    private String category;
+    private boolean isNew;
+
+    public MenuItem(double price, String description, String category, boolean isNew) {
+        this.price = price;
+        this.description = description;
+        this.category = category;
+        this.isNew = isNew;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setNew(boolean aNew) {
+        this.isNew = aNew;
+    }
+}
